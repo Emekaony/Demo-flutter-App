ac18181bb228d3c654a2b7f2a469b09326de036f
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-05-17 19:36:02 -0700

    Gotta set the theme manually

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,5 @@
+import "dart:developer";
+
 import "package:flutter/material.dart";
 
 void main() {
@@ -5,20 +7,88 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  MyApp({super.key});
+
+  bool isRunning = false;
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.deepPurple,
+          brightness: Brightness.dark, // this is how u change the brightness
+        ),
+        appBarTheme: AppBarTheme(backgroundColor: Colors.deepPurple),
+        floatingActionButtonTheme: FloatingActionButtonThemeData(
+          backgroundColor: Colors.deepPurple,
+        ),
+      ),
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            print("Floating action button was pressed!");
+          },
+          child: Icon(Icons.add),
+        ),
         appBar: AppBar(
-          title: Text("Simple App"),
-          backgroundColor: Colors.amber,
+          title: Text(
+            "Simple App",
+            style: TextStyle(color: Colors.white, fontWeight: FontWeight.bold),
+          ),
+          // backgroundColor: Colors.black,
           centerTitle: true,
         ),
         body: Container(
           padding: EdgeInsets.only(left: 10, right: 10, top: 10),
-          child: Center(child: Text("Welcome to my simple Application")),
+          child: Center(
+            child: Row(
+              children: [
+                TextButton(
+                  onPressed: () => {},
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: Colors.black,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Text(
+                      "RESET",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.grey,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+                Spacer(),
+                InkWell(
+                  splashColor: Colors.white,
+                  child: Container(
+                    alignment: Alignment.center,
+                    width: 100,
+                    height: 100,
+                    decoration: BoxDecoration(
+                      color: isRunning ? Colors.red : Colors.green,
+                      borderRadius: BorderRadius.circular(50),
+                    ),
+                    child: Text(
+                      isRunning ? "STOP" : "START",
+                      style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.white,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
         ),
       ),
     );
