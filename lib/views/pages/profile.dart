0419e7718d4126c625c2aa2afbb519f5d007b62b
Nnaemeka <Nnaemekaonyeokoro@outlook.com>
2025-05-21 09:32:09 -0700

    Almost done with this boring course

--- lib/views/pages/profile.dart
+++ lib/views/pages/profile.dart
@@ -1,10 +1,48 @@
 import 'package:flutter/material.dart';
 
-class ProfilePage extends StatelessWidget {
+class ProfilePage extends StatefulWidget {
   const ProfilePage({super.key});
 
   @override
+  State<ProfilePage> createState() => _ProfilePageState();
+}
+
+class _ProfilePageState extends State<ProfilePage> {
+  final TextEditingController controller = TextEditingController();
+  bool? tt = false;
+
+  @override
   Widget build(BuildContext context) {
-    return Center(child: Text("Profile page"));
+    return Container(
+      padding: EdgeInsets.all(10.0),
+      child: Column(
+        children: [
+          TextField(
+            controller: controller,
+            decoration: InputDecoration(border: OutlineInputBorder()),
+            onEditingComplete: () {
+              setState(() {});
+            },
+          ),
+          Padding(
+            padding: EdgeInsets.only(top: 10),
+            child: Align(
+              alignment: Alignment.centerLeft,
+              child: Text("Text: ${controller.text}"),
+            ),
+          ),
+          Checkbox.adaptive(
+            value: tt,
+            onChanged: (bool? value) {
+              setState(() {
+                if (tt != null) {
+                  tt = !tt!;
+                }
+              });
+            },
+          ),
+        ],
+      ),
+    );
   }
 }
